a062f28abe70c88b0cb46d00da17408b160d3924
Peter Gemmell <petergemmell65@hotmail.com>
2020-10-06 14:29:00 +0100

    imported beautiful soup allowing us to now parse the data

--- scraper.py
+++ scraper.py
@@ -1,7 +1,15 @@
 import requests
-# Essentially allows us to access a URl/ URL's
+# Essentially allows us to access a URl/ URL's.
+from bs4 import BeautifulSoup
+# BeautifulSoup allows us to parse that website data.
 
 URL = 'https://www.amazon.co.uk/Canon-1-2L-USM-Lens-Black/dp/B000I2J2S6/ref=sr_1_5?crid=2MWU8TLJB6W7V&dchild=1&keywords=canon+1.4+50mm&qid=1601990115&sprefix=canon+1.4%2Caps%2C147&sr=8-5'
 
-# Giving some information about our browser
+# Giving some information about our browser.
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
+
+# This is collecting ll that data from the website.
+page = requests.get(URL, headers=headers)
+
+# Here we create another variable called soup and pass in two arguments.
+soup = BeautifulSoup(page.content, 'html.parser')
